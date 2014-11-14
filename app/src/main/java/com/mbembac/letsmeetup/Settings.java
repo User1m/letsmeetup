9496c9a73d1a2b2b386ffa2adbf5d12c62930cc5
Amna Khan <khan.261@osu.edu>
2014-11-14 09:30:06 -0500

    working on settings

--- app/src/main/java/com/mbembac/letsmeetup/Settings.java
+++ app/src/main/java/com/mbembac/letsmeetup/Settings.java
@@ -1,31 +1,10 @@
 package com.mbembac.letsmeetup;
 
 import android.app.Fragment;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ListView;
-import android.widget.ArrayAdapter;
-import android.widget.Toast;
-
-import com.parse.FindCallback;
-import com.parse.ParseException;
-import com.parse.ParseQuery;
-
-import android.app.AlertDialog;
-import android.content.DialogInterface;
-
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
-
-import java.util.List;
-import java.util.ArrayList;
-
-import com.parse.ParseUser;
 
 public class Settings extends Fragment {
 
@@ -37,5 +16,5 @@ public class Settings extends Fragment {
         return v;
 
     }
-}
 
+}
